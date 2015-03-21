66e0db2de5c19143a26f1f4e3e651cd951db7cc8
Thomas Tuts <thomas.tuts@gmail.com>
2015-03-21 19:32:47 +0100

    Add fallback method for content parser if there are no paragraphs

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var parser = require('./lib/parser');
+var fs = require('fs');
+var path = require('path');
 
 parser.parse('http://techcrunch.com/2015/03/20/from-the-8200-to-silicon-valley/', function (err, data) {
-  console.log(data);
+  fs.writeFileSync(path.join(process.cwd(), 'data/techcrunch.html'), data.content);
+
+  parser.parse('http://paulgraham.com/altair.html', function (err, data) {
+    fs.writeFileSync(path.join(process.cwd(), 'data/altair.html'), data.content);
+  });
 });
