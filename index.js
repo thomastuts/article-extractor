a10bd4a07bbfe3e4c8f1c0ad353523db660dfb84
Thomas Tuts <thomas.tuts@gmail.com>
2015-03-21 19:44:19 +0100

    Expand parsing logic for articles

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ var parser = require('./lib/parser');
 var fs = require('fs');
 var path = require('path');
 
-parser.parse('http://techcrunch.com/2015/03/20/from-the-8200-to-silicon-valley/', function (err, data) {
-  fs.writeFileSync(path.join(process.cwd(), 'data/techcrunch.html'), data.content);
-
-  parser.parse('http://paulgraham.com/altair.html', function (err, data) {
-    fs.writeFileSync(path.join(process.cwd(), 'data/altair.html'), data.content);
-  });
+parser.parse('http://www.wired.com/2015/03/google-android-broken-wifi/', function (err, data) {
+  fs.writeFileSync(path.join(process.cwd(), 'data/wired.html'), data.content);
 });
