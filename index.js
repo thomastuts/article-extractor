bd64af2ba146eec89500e51e3013fb0f5ba4a4b2
Thomas Tuts <thomas.tuts@gmail.com>
2015-03-22 22:10:30 +0100

    Simplify project structure; remove webshot dependency

--- index.js
+++ index.js
@@ -1,47 +1,23 @@
-var parser = require('./lib/parser');
-var fs = require('fs');
-var path = require('path');
-var async = require('async');
+var request = require('request');
+var cleaner = require('./lib/cleaner');
+var author = require('./lib/parser/author');
+var content = require('./lib/parser/content');
+var title = require('./lib/parser/title');
+var summary = require('./lib/parser/summary');
 
-var articlesToParse = [
-  {
-    url: 'http://gizmodo.com/watch-a-single-day-on-the-london-tube-in-two-minutes-1692810056',
-    filename: 'gizmodo'
-  },
-  {
-    url: 'http://www.wired.com/2015/03/google-android-broken-wifi/',
-    filename: 'wired'
-  },
-  {
-    url: 'http://techcrunch.com/2015/03/20/from-the-8200-to-silicon-valley/',
-    filename: 'techcrunch'
-  },
-  {
-    url: 'http://paulgraham.com/altair.html',
-    filename: 'paulgraham'
-  },
-  {
-    url: 'http://www.economist.com/news/united-states/21646763-when-lethal-injection-gets-tricky-try-guns-or-gas-drugs-dont-work',
-    filename: 'the-economist'
-  },
-  {
-    url: 'http://onstartups.com/insider-look-at-hubspot-sidekick-growth-approach',
-    filename: 'on-startups'
-  },
-  {
-    url: 'http://www.smashingmagazine.com/2015/03/20/better-browser-input-events/',
-    filename: 'smashing-magazine'
+module.exports = {
+  extractData: function (url, callback) {
+    request(url, function (err, response, body) {
+      var preppedHtml = cleaner.prepForParsing(body);
+
+      var data = {
+        author: author.getAuthor(preppedHtml),
+        content: content.getArticleContent(preppedHtml),
+        title: title.getTitle(preppedHtml),
+        summary: summary.getSummary(preppedHtml)
+      };
+
+      callback(null, data);
+    });
   }
-];
-
-async.each(articlesToParse, function (articleToParse, parseCallback) {
-  parser.parse(articleToParse.url, function (err, data) {
-    console.log('Parsed article:', data.title);
-    console.log(data.summary);
-    console.log('-----');
-    fs.writeFileSync(path.join(process.cwd(), 'data/' + articleToParse.filename + '.html'), data.content);
-    parseCallback();
-  });
-}, function () {
-  process.exit(0);
-});
+}
